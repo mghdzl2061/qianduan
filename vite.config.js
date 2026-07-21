603c35d0fe74cc6108815e1188375e962465c1dc
liucang <2767944710@qq.com>
2026-07-21 12:04:14 +0800

    内网穿透允许不同网络的设备也可以访问网址

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,17 @@ export default defineConfig({
   server: {
     port: 5173,
     host: '0.0.0.0',
+    // cpolar 每次可能分配不同的临时域名，允许该公网 Host 访问开发服务器。
+    allowedHosts: true,
+    proxy: {
+      '/api': 'http://127.0.0.1:8080',
+      '/uploads': 'http://127.0.0.1:8080'
+    }
+  },
+  preview: {
+    port: 5173,
+    host: '0.0.0.0',
+    allowedHosts: true,
     proxy: {
       '/api': 'http://127.0.0.1:8080',
       '/uploads': 'http://127.0.0.1:8080'
